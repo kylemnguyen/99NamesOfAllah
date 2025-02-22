82305694804e55e16ce937653e48293dfb0e7090
kylemnguyen <kylemtnguyen@gmail.com>
2025-02-21 22:39:21 -0700

    Adding all files

---
+++ 99names/main.js
@@ -0,0 +1,122 @@
+arabic = [
+    "ٱلرَّحْمَٰنُ", "ٱلرَّحِيمُ", "ٱلْمَلِكُ",
+    "ٱلْقُدُّوسُ", "ٱلسَّلَامُ", "ٱلْمُؤْمِنُ",
+    "ٱلْمُهَيْمِنُ", "ٱلْعَزِيزُ", "ٱلْجَبَّارُ",
+    "ٱلْمُتَكَبِّرُ", "ٱلْخَٰلِقُ", "ٱلْبَارِئُ",
+    "ٱلْمُصَوِّرُ", "ٱلْغَفَّارُ", "ٱلْقَهَّارُ",
+    "ٱلْوَهَّابُ", "ٱلْرَّزَّاقُ", "ٱلْفَتَّاحُ",
+    "ٱلْعَلِيمُ", "ٱلْقَابِضُ", "ٱلْبَاسِطُ",
+    "ٱلْخَافِضُ", "ٱلْرَّافِعُ", "ٱلْمُعِزُّ",
+    "ٱلْمُذِلُّ", "ٱلسَّمِيعُ", "ٱلْبَصِيرُ",
+    "ٱلْحَكَمُ", "ٱلْعَدْلُ", "ٱللَّطِيفُ",
+    "ٱلْخَبِيرُ", "ٱلْحَلِيمُ", "ٱلْعَظِيمُ",
+    "ٱلْغَفُورُ", "ٱلشَّكُورُ", "ٱلْعَلِيُّ",
+    "ٱلْكَبِيرُ", "ٱلْحَفِيظُ", "ٱلْمُقيِتُ",
+    "ٱلْحَسِيبُ", "ٱلْجَلِيلُ", "ٱلْكَرِيمُ",
+    "ٱلْرَّقِيبُ", "ٱلْمُجِيبُ", "ٱلْوَاسِعُ",
+    "ٱلْحَكِيمُ", "ٱلْوَدُودُ", "ٱلْمَجِيدُ",
+    "ٱلْبَاعِثُ", "ٱلْشَّهِيدُ", "ٱلْحَقُّ",
+    "ٱلْوَكِيلُ", "ٱلْقَوِيِيُ", "ٱلْمَتِينُ",
+    "ٱلْوَلِيُ", "ٱلْحَمِيدُ", "ٱلْمُحْصِي",
+    "ٱلْمُبْدِئُ", "ٱلْمُعِيدُ", "ٱلْمُحْيِي",
+    "ٱلْمُمِيتُ", "ٱلْحَيِّيُ", "ٱلْقَيُّومُ",
+    "ٱلْوَاجِدُ", "ٱلْمَاجِدُ", "ٱلْوَاحِدُ",
+    "ٱلْأَحَدُ", "ٱلصَّمَدُ", "ٱلْقَادِرُ",
+    "ٱلْمُقْتَدِرُ", "ٱلْمُقَدِّمُ", "ٱلْمُؤَخِّرُ",
+    "ٱلْأوَّلُ", "ٱلْآخِرُ", "ٱلظَّٰهِرُ",
+    "ٱلْبَاطِنُ", "ٱلْوَالِي", "ٱلْمُتَعَالِي",
+    "ٱلْبَرُّ", "ٱلتَّوَابُ", "ٱلْمُنْتَقِمُ",
+    "ٱلْعَفُوُ", "ٱلْرَّؤُوفُ", "مَالِكُ ٱلْمُلْكُ",
+    "ذُوالْجَلاَلِ وَالإكْرَامِ", "ٱلْمُقْسِطُ", "ٱلْجَامِعُ",
+    "ٱلْغَنيُّ", "ٱلْمُغْنِيُ", "ٱلْمَانِعُ",
+    "ٱلضَّٰرَ", "ٱلنَّافِعُ", "ٱلْنُورُ",
+    "ٱلْهَادِي", "ٱلْبَدِيعُ", "ٱلْبَاقِي",
+    "ٱلْوَارِثُ", "ٱلرَّشِيدُ", "ٱلصَّبُورُ",
+]
+
+arabicEnglish = [
+    "Ar-Rahmaan", "Ar-Raheem", "Al-Malik",   
+    "Al-Quddus", "As-Salaam", "Al-Mu'min",   
+    "Al-Muhaymin", "Al-Azeez", "Al-Jabbaar",   
+    "Al-Mutakabbir", "Al-Khaaliq", "Al-Baari'",   
+    "Al-Musawwir", "Al-Ghaffaar", "Al-Qahhaar",   
+    "Al-Wahhaab", "Ar-Razzaaq", "Al-Fattah",   
+    "Al-'Aleem", "Al-Qaabid", "Al-Baasit",   
+    "Al-Khaafidh", "Ar-Raafi'", "Al-Mu'izz",   
+    "Al-Muzil", "As-Samee'", "Al-Baseer",   
+    "Al-Hakam", "Al-'Adl", "Al-Lateef",   
+    "Al-Khabeer", "Al-Haleem", "Al-'Azeem",   
+    "Al-Ghafoor", "Ash-Shaakoor", "Al-'Alee",   
+    "Al-Kabeer", "Al-Hafeedh", "Al-Muqeet",   
+    "Al-Hasib", "Al-Jaleel", "Al-Kareem",   
+    "Ar-Raqeeb", "Al-Mujeeb", "Al-Waasi'",   
+    "Al-Hakeem", "Al-Wadud", "Al-Majeed",   
+    "Al-Baa'ith", "As-Shaheed", "Al-Haqq",   
+    "Al-Wakeel", "	Al-Qawiyy", "Al-Mateen",   
+    "Al-Walee", "Al-Hameed", "Al-Muhsee",   
+    "Al-Mubdi'", "Al-Mu'id", "Al-Muhyee",   
+    "Al-Mumeet", "Al-Hayy", "Al-Qayyoom",   
+    "Al-Waajid", "Al-Maajid", "Al-Waahid",   
+    "Al-Ahad", "As-Samad", "Al-Qaadir",   
+    "Al-Muqtadir", "Al-Muqaddim", "Al-Mu'akhkhir",   
+    "Al-Awwal", "Al-Aakhir", "Az-Zaahir",   
+    "Al-Baatin", "Al-Waalee", "Al-Muta'ali",   
+    "Al-Barr", "At-Tawwaab", "Al-Muntaqim",   
+    "Al-'Afuww", "Ar-Ra'ooof", "Maalik-al-Mulk",   
+    "Thul-Jalaali wal-Ikraam", "Al-Muqsiṭ", "Al-Jaami'",   
+    "Al-Ghaniyy", "Al-Mughniyy", "Al-Maani'",   
+    "Ad-Dhaarr", "An-Naafi'", "An-Noor",   
+    "Al-Haadi", "Al-Badee'", "Al-Baaqi",   
+    "Al-Waarith", "Ar-Rasheed", "Al-Saboor"
+]
+
+english = [
+    "The Most or Entirely Merciful", "The Bestower of Mercy", "	The King and Owner of Dominion",
+    "The Absolutely Pure", "The Perfection and Giver of Peace", "The One Who gives Emaan and Security",
+    "The Guardian, The Witness, The Overseer", "The All Mighty", "The Compeller, The Restorer",
+    "The Supreme, The Majestic", "The Creator, The Maker", "The Originator",
+    "The Fashioner", "The All- and Oft-Forgiving", "The Subduer, The Ever-Dominating",
+    "The Giver of Gifts", "The Provider", "The Opener, The Judge",
+    "The All-Knowing, The Omniscient", "The Withholder", "The Extender",
+    "The Reducer, The Abaser", "The Exalter, The Elevator", "The Honourer, The Bestower",
+    "The Dishonourer, The Humiliator", "The All-Hearing", "The All-Seeing",
+    "The Judge, The Giver of Justice", "The Utterly Just", "The Subtle One, The Most Gentle",
+    "The Acquainted, The All-Aware", "The Most Forbearing", "The Magnificent, The Supreme",
+    "The Forgiving, The Exceedingly Forgiving", "The Most Appreciative", "The Most High, The Exalted",
+    "The Greatest, The Most Grand", "The Preserver, The All-Heedful and All-Protecting", "The Sustainer, The Maintainer",
+    "The Reckoner", "The Majestic", "The Most Generous, The Most Esteemed",
+    "The Watchful, The All-Watchful", "The Responsive, The Answerer", "The All-Encompassing, the Boundless",
+    "The All-Wise", "The Most Loving", "The Glorious, The Most Honorable",
+    "The Infuser of New Life", "The All Observing Witnessing", "The Absolute Truth",
+    "The Trustee, The Disposer of Affairs", "The All-Strong", "The Firm, The Steadfast",
+    "The Protecting Associate", "The Praiseworthy", "The All-Enumerating, The Counter",
+    "The Originator, The Initiator", "The Restorer, The Reinstater", "The Giver of Life",
+    "The Creator of Death", "The Ever-Living", "The Sustainer, The Self-Subsisting",
+    "The Perceiver", "The Illustrious, The Magnificent", "The One, The Indivisible",
+    "The Unique, The Only One", "The Eternal, Satisfier of Needs", "The Omnipotent",
+    "The Powerful", "The Expediter, The Promoter", "The Delayer",
+    "The First", "The Last, The Utmost", "The Manifest, The All-Surpassing",
+    "The Hidden One, Knower of the Hidden", "The Sole Governor", "The Self Exalted",
+    "The Source of All Goodness", "The Ever-Pardoning", "The Avenger",
+    "The Pardoner", "The Most Kind", "Master of the Dominion, Owner of the Kingdom",
+    "Possessor of Glory and Honour", "The Just One", "The Gatherer, the Uniter",
+    "The Self-Sufficient, The Wealthy", "The Enricher", "The Withholder",
+    "The Distresser", "The Propitious, The Benefactor", "The Light",
+    "The Guide", "The Incomparable Originator", "The Ever-Lasting",
+    "The Inheritor", "The Guide to the Right Path", "The Patient"
+
+]
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  
+const randomNumber = getRandomNumber(0, 98);
+
+document.getElementById("arabic").innerHTML = arabic[randomNumber];
+document.getElementById("english-writing").innerHTML = arabicEnglish[randomNumber];
+
+document.getElementById("reveal").addEventListener("click", function() {
+    document.getElementById("english-meaning").innerHTML = english[randomNumber];
+
+});
